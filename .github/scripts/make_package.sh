1815c23f5e2d16cd2ab3606ed2357bcd92d7cc66
cakevm <cakevm@proton.me>
2025-03-30 17:31:11 +0200

    Build for browser only

--- .github/scripts/make_package.sh
+++ .github/scripts/make_package.sh
@@ -12,21 +12,6 @@ if [ -d "pkg" ]; then
     rm -rf pkg
 fi
 
-if [ -d "pkg-node" ]; then
-    rm -rf pkg-node
-fi
-
-# Build for both targets
+# Build for browser only
 export RUSTFLAGS='--cfg getrandom_backend="wasm_js"'
-wasm-pack build -t nodejs -d pkg-node crates/js
 wasm-pack build -t browser -d pkg crates/js
-
-# Get the package name
-PKG_NAME=$(jq -r .name crates/js/pkg/package.json | sed 's/\-/_/g')
-
-# Give the packages a version tag
-if [ -n "$VERSION_TAG" ]; then
-    # Overwrite the version in the package.json
-    jq --arg v $VERSION_TAG '.version = $v' crates/js/pkg/package.json > temp.json && mv temp.json crates/js/pkg/package.json
-    jq --arg v $VERSION_TAG '.version = $v' crates/js/pkg-node/package.json > temp.json && mv temp.json crates/js/pkg-node/package.json
-fi
