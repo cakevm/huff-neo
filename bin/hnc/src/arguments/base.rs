631e622ef1b26632358521920ec4e449cdf50f88
cakevm <cakevm@proton.me>
2025-08-16 10:41:56 +0200

    better help

--- bin/hnc/src/arguments/base.rs
+++ bin/hnc/src/arguments/base.rs
@@ -85,13 +85,13 @@ pub struct HuffArgs {
     #[clap(subcommand)]
     pub test: Option<TestCommands>,
 
+    /// Output the flattened source code with all dependencies resolved
+    #[clap(long = "flattened-source")]
+    pub flattened_source: bool,
+
     /// Print version
     #[arg(short = 'V', long = "version")]
     pub version_long: bool,
-
-    /// Output the flattened source code
-    #[clap(long = "flattened-source")]
-    pub flattened_source: bool,
 }
 
 #[derive(Subcommand, Clone, Debug)]
